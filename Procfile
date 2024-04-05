1457098bce1b6d2fcab683ae560efddab942fc9c
Erick Martins <dkzera10@gmail.com>
2024-04-04 21:55:00 -0300

    Deploy Conf

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn inforedeti.wsgi --log-file -
--- inforedeti/settings.py
+++ inforedeti/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,3 +133,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'login/'
+
+django_heroku.settings(locals())
